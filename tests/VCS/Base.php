65f9ac5650edba819690359bc70957012a75ed95
Hemachandar <hmac.devo@gmail.com>
2026-02-25 17:01:06 +0530

    env var

--- tests/VCS/Base.php
+++ tests/VCS/Base.php
@@ -51,7 +51,7 @@ abstract class Base extends TestCase
 
     public function testSearchRepositories(): void
     {
-        $installationId = System::getEnv('INSTALLATION_ID') ?? '';
+        $installationId = System::getEnv('TESTS_GITHUB_INSTALLATION_ID') ?? '';
         ['items' => $repos, 'total' => $total] = $this->vcsAdapter->searchRepositories($installationId, 'test-kh', 1, 2);
         $this->assertCount(2, $repos);
         $this->assertSame(6, $total);
